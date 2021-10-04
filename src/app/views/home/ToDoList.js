9997027f14b87fad6e7cfd94f86f71590efeb59f
anmi <>
2021-10-04 13:46:18 +0800

    10/04-01

--- src/app/views/home/ToDoList.js
+++ src/app/views/home/ToDoList.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react'
 
 const ToDoList = (props) => {
-    const [addData, handleChange] = useState(null)
+    const [addData, handleChange] = useState('')
     const [todo, setTodo] = useState([])
+    const [todoObj, setObj] = useState([])
 
     useEffect(() => {
         setTodo(JSON.parse(localStorage.getItem('listData')) || [])
     }, [])
-
     useEffect(() => {
         localStorage.setItem('listData', JSON.stringify(todo))
         console.log('todo', todo)
     }, [todo])
+    useEffect(() => {
+        console.log('todoObj', todoObj)
+    }, [todoObj])
 
     return (
         <div className="p-4">
@@ -23,32 +26,54 @@ const ToDoList = (props) => {
                     placeholder="輸入代辦事項"
                     onChange={(e) => handleChange(e.target.value)}
                 />
+
                 <button
                     className="btn rounded"
                     onClick={() => {
+                        const todoProxy = { ...todoObj }
+                        todoProxy.content = addData
+                        todoProxy.isCompleted = false
+                        setObj(todoProxy)
                         setTodo((arr) => {
-                            return [...arr, addData]
+                            return [...arr, todoProxy]
                         })
-                        handleChange(' ')
+
+                        handleChange('')
                     }}
+                    disabled={!addData}
                 >
                     新增
                 </button>
             </div>
             <div className="d-flex my-1">待辦事項</div>
             <div className="border mb-2"></div>
+
             <ul>
                 {todo.map((item, i) => (
                     <li key={i} className="d-flex justify-content-between mb-2">
-                        {item}
+                        <div className="d-flex align-items-center">
+                            <input
+                                type="checkbox"
+                                className="hide-switch"
+                                onChange={(e) => {
+                                    const id = e.target.value
+                                    const idTodo = [...todo]
+                                    console.log('idTodo[id]', idTodo[id])
+                                    idTodo[id].isCompleted = !idTodo[id].isCompleted
+                                    setTodo(idTodo)
+                                }}
+                                checked={item.isCompleted}
+                                value={i}
+                            />
+                            <label className="btn btn-select border-0">{item.content}</label>
+                        </div>
                         <button
                             key={i}
-                            className="btn"
+                            className="btn rounded"
                             onClick={() => {
                                 // const todoProxy = [...todo]
                                 // todoProxy.splice(i, 1)
-                                const todoProxy = [...todo].slice(0, i).concat([...todo].slice(i + 1))
-                                setTodo(todoProxy)
+                                setTodo((arr) => [...arr.slice(0, i), ...arr.slice(i + 1)])
                             }}
                         >
                             刪除
